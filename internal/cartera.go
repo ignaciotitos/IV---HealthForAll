5f003cc58afd3b700445611349ac07589010912b
manujurado1 <manujurado1@correo.ugr.es>
2022-12-19 12:46:13 +0100

    closes #17 porque se ha añadido un atributo al modelo entidad para cumplir la HU guardando la información que le permita saber el dinero que tendrá en el futuro

--- internal/cartera.go
+++ internal/cartera.go
@@ -1,5 +1,6 @@
 package internal
 
 type Cartera struct {
-	ListaMonedas []*Moneda //Lista con todas las monedas que posee el usuario en su propiedad
+	ListaMonedas      []*Moneda          //Lista con todas las monedas que posee el usuario en su propiedad
+	PrediccionMonedas map[string]float32 //Diccionario dónde a cada moneda se le asocia un % (positivo o negativo) de la estimación de modificación del valor de esa moneda en el futuro
 }
