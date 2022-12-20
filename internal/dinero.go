a30e9d030c5762d557b42ab305414fc8e7127752
manujurado1 <manujurado1@correo.ugr.es>
2022-12-20 21:26:17 +0100

    deshacer #21 #22 y #23 ya que estaban mal planteados

--- internal/dinero.go
+++
@@ -1,7 +0,0 @@
-package internal
-
-type Dinero struct {
-	Valor     float64 //Indicador que refleja el valor actual
-	Cantidad  float32 //Cantidad de dinero que posee el usuario
-	Inflacion float64 //Parámetro que indica la inflación que tendrá el dinero
-}
