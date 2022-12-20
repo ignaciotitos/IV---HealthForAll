ede3aef8f58d0d8c12f836f169665f7ecb9c6e41
manujurado1 <manujurado1@correo.ugr.es>
2022-12-20 17:41:43 +0100

    deshacer #17 #18 y #19 debido a que son issues inválidos

--- internal/cartera.go
+++
@@ -1,6 +0,0 @@
-package internal
-
-type Cartera struct {
-	ListaMonedas      []*Moneda          //Lista con todas las monedas que posee el usuario en su propiedad
-	PrediccionMonedas map[string]float32 //Diccionario dónde a cada moneda se le asocia un % (positivo o negativo) de la estimación de modificación del valor de esa moneda en el futuro
-}
