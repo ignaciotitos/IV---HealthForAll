ca8c7ddb5ee57472c62aebdd70b915448b156b8f
manujurado1 <manujurado1@correo.ugr.es>
2022-12-21 14:06:19 +0100

    closes #23 porque se ha añadido a la estructura de moneda una estructura que recoge todos los atributos necesarios para calcular la variación de valor de una moneda

--- internal/moneda.go
+++ internal/moneda.go
@@ -1,8 +1,16 @@
 package internal
 
 type Moneda struct {
-	ValorActual float64
-	Cantidad    float32
-	Oferta      float64
-	Demanda     float64
+	ValorActual         float64
+	Cantidad            float32
+	DatosMercadoDivisas struct {
+		NivelRentaNacional         float64
+		PreciosRelativosBienes     float64
+		NivelInversionDomestico    float64
+		DiferencialIntereses       float64
+		SaldoBalanzaPagos          float64
+		RentaMundial               float64
+		PreciosRelativos           float64
+		CalidadProducciónDoméstica float64
+	}
 }
