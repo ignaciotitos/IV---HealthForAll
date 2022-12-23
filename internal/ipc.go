85395f7edbb6a5f7d595bdb68c539724f1eeef48
manujurado1 <manujurado1@correo.ugr.es>
2022-12-23 14:05:19 +0100

    closes #25 porque se ha creado un objeto valor que soluciona el issue de como calcular el IPC

---
+++ internal/ipc.go
@@ -0,0 +1,8 @@
+package internal
+
+type IPC struct {
+	AnioBase                       int
+	AnioAComparar                  int
+	PrecioCestaBienesAnioBase      float32
+	PrecioCestaBienesAnioAComparar float32
+}
