ce424f49f4451b6070b9cf4ad1b9fbb0a5af4f7e
manujurado1 <manujurado1@correo.ugr.es>
2022-12-28 17:05:33 +0100

    Deshacer cambios relacionados con issues inválidos

--- internal/prediccion.go
+++
@@ -1,7 +0,0 @@
-package internal
-
-type Prediccion struct {
-	Mes            int
-	Anio           int
-	TasasInflacion []*TasaInflacion
-}
--- internal/tasaInflacion.go
+++
@@ -1,7 +0,0 @@
-package internal
-
-type TasaInflacion struct {
-	Mes   int
-	Anio  int
-	Valor float32
-}
