01381d15bf8538e25da12845bfa3ca1704bf41ad
manujurado1 <manujurado1@correo.ugr.es>
2022-12-28 17:11:35 +0100

    closes #30 porque se ha implementado un constructor que evita fechas inválidas

--- internal/tasaInflacionMensual.go
+++ internal/tasaInflacionMensual.go
@@ -1,7 +1,17 @@
 package internal
 
+import "errors"
+
 type TasaInflacionMensual struct {
 	Mes   uint8
 	Anio  uint16
 	Valor float32
+}
+
+func NuevaTasaInflacionMensual(Mes uint8, Anio uint16, Valor float32) (*TasaInflacionMensual, error) {
+	if 1 <= Mes && Mes <= 12 && 2012 <= Anio && Anio <= 2022 {
+		return &TasaInflacionMensual{Mes, Anio, Valor}, nil
+	}
+
+	return nil, errors.New("Ya existe una tasa de inflación con esa fecha")
 }
