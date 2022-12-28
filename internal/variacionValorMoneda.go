6d40ab34843256d6dd6748b990b04bbdf39a3854
manujurado1 <manujurado1@correo.ugr.es>
2022-12-28 17:15:43 +0100

    closes #31 porque ya no se permiten tasas de inflación con la misma fecha
    Se ha creado una función para añadir tasas de inflación a la lista propia del atributo de la entidad que comprueba que ese par de valores (mes,año) no está ya añadido

--- internal/variacionValorMoneda.go
+++ internal/variacionValorMoneda.go
@@ -3,3 +3,19 @@ package internal
 type VariacionValorMoneda struct {
 	TasasInflacionMesesAnteriores []*TasaInflacionMensual
 }
+
+func (this VariacionValorMoneda) añadirTasaAListaDeTasas(tasa *TasaInflacionMensual) {
+	if fechaExistenteEnLista(this.TasasInflacionMesesAnteriores, tasa) == false {
+		this.TasasInflacionMesesAnteriores = append(this.TasasInflacionMesesAnteriores, tasa)
+	}
+
+}
+
+func fechaExistenteEnLista(lista []*TasaInflacionMensual, tasa *TasaInflacionMensual) bool {
+	for _, tasaLista := range lista {
+		if (tasaLista.Mes == tasa.Mes) && (tasaLista.Anio == tasa.Anio) {
+			return true
+		}
+	}
+	return false
+}
